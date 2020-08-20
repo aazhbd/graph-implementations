46a42a565816d4fe4f785f164616362aed27d800
AAZ H <aazhbd@yahoo.com>
2020-08-20 02:53:42 +0200

    Updating greedy coloring code.

--- GreedyColoringJava/src/GreedyColoring.java
+++ GreedyColoringJava/src/GreedyColoring.java
@@ -3,6 +3,18 @@ import java.io.*;
 
 public class GreedyColoring {
     public static final String input = "in.txt";
+    private HashMap<Integer, List<Integer>> graph;
+    private List<Integer> vertices;
+
+    public GreedyColoring(Integer v[]) {
+        this.vertices = Arrays.asList(v);
+
+        this.graph = new HashMap<Integer, List<Integer>>();
+
+        for(int vertex : this.vertices) {
+            this.graph.put(vertex, new ArrayList<Integer>());
+        }
+    }
 
     public static void main(String args[]) {
         FileInputStream instream = null;
