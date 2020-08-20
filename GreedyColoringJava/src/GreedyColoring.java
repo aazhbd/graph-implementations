9ee398d0dc503d5848a946308d22a61172d1418a
AAZ H <aazhbd@yahoo.com>
2020-08-20 02:36:17 +0200

    Updating greedy coloring code.

---
+++ GreedyColoringJava/src/GreedyColoring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class GreedyColoring {
+    public static final String input = "in.txt";
+
+    public static void main(String args[]) {
+        FileInputStream instream = null;
+
+    }
+}
