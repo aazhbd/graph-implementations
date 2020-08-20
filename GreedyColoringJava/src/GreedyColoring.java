220f33c9930da8e73e4c3e3e35a740d5c1da7fba
AAZ H <aazhbd@yahoo.com>
2020-08-20 07:54:49 +0200

    Updating greedy coloring code.

--- GreedyColoringJava/src/GreedyColoring.java
+++ GreedyColoringJava/src/GreedyColoring.java
@@ -16,6 +16,46 @@ public class GreedyColoring {
         }
     }
 
+    public void addConnection(Integer f, Integer t) {
+        if(this.vertices.contains(f) && this.vertices.contains(t))
+        {
+            List<Integer> edges = this.graph.get(f);
+
+            if(!edges.contains(t)) {
+                edges.add(t);
+            }
+
+            this.graph.put(f, edges);
+        }
+    }
+
+    public int pickColor(List<Integer> colors) {
+        for(int c = 0; ; c++) {
+            if(!colors.contains(c)) {
+                return c;
+            }
+        }
+    }
+
+    public HashMap<Integer, Integer> applyColors() {
+        HashMap<Integer, Integer> colored = new HashMap<Integer, Integer>();
+        List<Integer> used_colors = new ArrayList<Integer>();
+
+        for(int vertex : this.vertices) {
+            List<Integer> edges = this.graph.get(vertex);
+
+            for(int e : edges) {
+                if(colored.containsKey(e)) {
+                    used_colors.add(colored.get(e));
+                }
+            }
+
+            colored.put(vertex, pickColor(used_colors));
+        }
+
+        return colored;
+    }
+
     public static void main(String args[]) {
         FileInputStream instream = null;
 
