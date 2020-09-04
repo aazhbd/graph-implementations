5a6a35ab139fb4bfaae635a8dfbd52ed47f5097f
AAZ H <aazhbd@yahoo.com>
2020-09-04 08:11:28 +0200

    Prolog code updated.

--- graphcoloringpl/graphrepresentation.pl
+++ graphcoloringpl/graphrepresentation.pl
@@ -1,18 +1,18 @@
-coloring(A,B,C,D,E,F):-
-  connected(A,B),
-  connected(A,C),
-  connected(A,D),
-  connected(A,F),
-  connected(B,C),
-  connected(B,E),
-  connected(C,D),
-  connected(C,E),
-  connected(D,E),
-  connected(E,F).
+coloring(A, B, C, D, E, F):-
+  connected(A, B),
+  connected(A, C),
+  connected(A, D),
+  connected(A, F),
+  connected(B, C),
+  connected(B, E),
+  connected(C, D),
+  connected(C, E),
+  connected(D, E),
+  connected(E, F).
 
-connected(yellow,blue).
-connected(blue,yellow).
-connected(yellow,red).
-connected(red,yellow).
-connected(blue,red).
-connected(red,blue).
+connected(yellow, blue).
+connected(blue, yellow).
+connected(yellow, red).
+connected(red, yellow).
+connected(blue, red).
+connected(red, blue).
